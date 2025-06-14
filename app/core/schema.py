694648ab2e1a164106699b604a49818fd78ff6c8
aquarius1905 <reiko6122@gmail.com>
2025-06-14 21:35:47 +0900

    DataRecordクラス修正

--- app/core/schema.py
+++ app/core/schema.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel, Field
 from typing import List
 
 class DataRecord(BaseModel):
-    user_id: int = Field(..., example=123)
-    name: str = Field(..., example="山田太郎")
-    email: str = Field(..., example="taro@example.com")
+    user_id: int = Field(..., description="ユーザーID")
+    name: str = Field(..., description="ユーザー名")
+    email: str = Field(..., description="メールアドレス")
 
 class SQSMessageBody(BaseModel):
     records: List[DataRecord]
