62f37322982dbfe5802e46b0a694588a45d8c3c3
aquarius1905 <reiko6122@gmail.com>
2025-08-23 22:21:28 +0900

    本の記載通りに実装

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import os
 import pytest
 
 # モジュールレベルで環境変数を設定（importより前に実行される）
+# 注意：現在はMagicMockを使用するため実際にはLocalStackは使用しない
 os.environ.setdefault('AWS_ENDPOINT_URL', 'http://localhost:4566')
 os.environ.setdefault('AWS_REGION', 'us-east-1')
 
